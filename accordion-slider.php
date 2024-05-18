d48e0cc6d1fcfbb5572a8561b90bd8b75c8813a8
Grégory Parrot <gparrot@xternalsoft.com>
2024-05-18 21:52:45 +0200

    Forcage de la 1.10.0 en attendant la MAJ officielle

--- accordion-slider.php
+++ accordion-slider.php
@@ -4,7 +4,7 @@
 	Plugin Name: Accordion Slider
 	Plugin URI:  https://bqworks.net/accordion-slider/
 	Description: Responsive and touch-enabled accordion slider.
-	Version:     1.9.9
+	Version:     1.10.0
 	Author:      bqworks
 	Author URI:  https://bqworks.net
 */
